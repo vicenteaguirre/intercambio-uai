4ba3754b445890a1fabb325a0a4107896077a857
vicenteaguirre <vj.aguirresanchez@gmail.com>
2022-07-04 21:10:40 -0400

    INTER-47 Arreglar middleware del login

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,16 +1,51 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import TestimonioUAI from "../components/TestimonioSection/TestimonioUAI";
 import TestimonioInternacional from "../components/TestimonioSection/TestimonioInternacional";
 import TestimonioAdmin from "../components/TestimonioSection/TestimonioAdmin";
+import {Texto} from "../components/Aux/AuxElements"
+import { Link } from "react-router-dom";
+import axios from 'axios';
 
 const Admin = () => {
+  const [auth, setAuth] = useState(false);
+  const token = localStorage.getItem("token");
 
-  return (
-    <Fragment>
-        <TestimonioUAI showAdmin={true}/>
-        <TestimonioInternacional showAdmin={true}/>
-        <TestimonioAdmin/>
-    </Fragment>
-)};
+
+  const getAuth = async () => {
+    const getAllApiUrl = 'http://localhost:3001/verify'
+    axios
+    .get(getAllApiUrl,{ headers: { token: token } })
+    .then((response) => {
+      if(response.data===true){
+        setAuth(true);
+      }else{
+        setAuth(false);
+      }
+    })
+  }
+
+  useEffect(() => {
+    getAuth()
+  }, [])
+
+  if(auth===true){
+    return (
+      <Fragment>
+          <TestimonioUAI showAdmin={true}/>
+          <TestimonioInternacional showAdmin={true}/>
+          <TestimonioAdmin/>
+      </Fragment>
+    )
+  } else {
+    return(
+      <Fragment>
+          <Texto>Página no disponible, porfavor inicie sesión.</Texto>
+          <Link to="/login">
+            <Texto> Iniciar Sesión</Texto>
+          </Link>
+      </Fragment>
+    )
+  }
+};
 
 export default Admin;
